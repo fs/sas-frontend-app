79867ebd0ddfecf9fa240d454a7cf56718123652
Владислав Щербаков <v.shcherbakov.sas@gmail.com>
2022-11-25 10:32:20 +0500

    removed git-template-area, added media rule for  3-column grid

--- src/components/styles.js
+++ src/components/styles.js
@@ -2,33 +2,19 @@ import styled from "styled-components";
 
 export const List = styled.div`
   display: grid;
-  @media (max-width: 1500px) {
+  @media (max-width: 1100px) {
     grid-template-columns: 1fr 1fr;
     grid-template-rows: repeat(10, 1fr);
-    grid-template-areas:
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". ."
-      ". .";
-    gap: 20px 40px;
+  }
+  @media (min-width: 1100px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(7, 1fr);
   }
   @media (min-width: 1500px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-    grid-template-areas:
-      ". . . ."
-      ". . . ."
-      ". . . ."
-      ". . . ."
-      ". . . .";
-    gap: 20px 20px;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(5, 1fr);
   }
+  gap: 20px 20px;
   padding: 78px;
 `;
 
