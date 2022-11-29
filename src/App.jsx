c71e5019c61e68b9704ea753310c16a0445c0b8e
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-11-29 10:27:46 +0500

    App.jsx to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import SearchBox from "./components/SearchBox";
+import RecipesList from "./components/RecipesList";
 
 const App = () => {
-  return <SearchBox />;
+  return <RecipesList />;
 };
 
 export default App;
