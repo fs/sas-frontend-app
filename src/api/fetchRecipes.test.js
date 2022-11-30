43f954736f3f51b5c9503fc4898e765b5a0d7c83
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2022-11-30 14:27:26 +0300

    add test

---
+++ src/api/fetchRecipes.test.js
@@ -0,0 +1,14 @@
+import fetchRecipes from "./fetchRecipes";
+
+describe("fetchRecipes", () => {
+  test("should fetch recipes", () => {
+    // Arrange
+
+    // Act
+
+    const result = fetchRecipes();
+
+    // Assert
+    expect(1).toEqual(2);
+  });
+});
