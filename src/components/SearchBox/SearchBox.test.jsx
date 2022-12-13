96584bbbc06f61239722ac11594b13aefb612582
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-12-13 15:30:32 +0500

    Minor adjustments to SearchBox tests

--- src/components/SearchBox/SearchBox.test.jsx
+++ src/components/SearchBox/SearchBox.test.jsx
@@ -31,7 +31,7 @@ describe("RecipeList", () => {
 });
 
 describe("SearchBox", () => {
-  test("should create two validation errors", async () => {
+  test("should create errors", async () => {
     // Arrange
     const expectedIngredients = "";
     const expectedCalories = "-5000";
@@ -52,6 +52,30 @@ describe("SearchBox", () => {
     const errors = screen.getAllByTestId("test-validation-message");
 
     // Assert
-    expect(errors.length).toBe(2);
+    expect(errors.length).toBeGreaterThan(0);
+  });
+
+  test("should not create any validation errors", async () => {
+    // Arrange
+    const expectedIngredients = "Peach cream";
+    const expectedCalories = "2000";
+
+    render(<SearchBox />);
+    const form = screen.getByTestId("test-form");
+    const inputIngredients = screen.getByTestId("test-input-ingredients");
+    const inputCalories = screen.getByTestId("test-input-calories");
+
+    // Act
+    fireEvent.change(inputIngredients, {
+      target: { value: expectedIngredients },
+    });
+    fireEvent.change(inputCalories, { target: { value: expectedCalories } });
+
+    fireEvent.submit(form);
+
+    const errors = screen.getAllByTestId("test-validation-message");
+
+    // Assert
+    expect(errors.length).toBe(0);
   });
 });
